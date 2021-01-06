3b7d6f4f809d2fda4d600c8655b3143d5876ec9c
Nicko <nickoputra42@gmail.com>
2021-01-06 19:16:58 +0700

    installation example

---
+++ Installation/src/nachos/example/MySystem.java
@@ -0,0 +1,9 @@
+package nachos.example;
+
+public class MySystem {
+
+	public MySystem() {
+		System.out.println("My System");
+	}
+
+}
---
+++ Installation/src/nachos/proj1/MainSystem.java
@@ -0,0 +1,9 @@
+package nachos.proj1;
+
+public class MainSystem {
+
+	public MainSystem() {
+		System.out.println("Hello");
+	}
+
+}
